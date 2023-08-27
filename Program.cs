f4c7c46eb99620d1ed785cc55c47b4ae867a92f2
ChickenLittle02 <rubnmartinezrojas@gmail.com>
2023-08-27 16:37:23 -0400

    Agregando el inicio del Tokenizer del Interprete

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Run
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Escriba su codigo");
+            string texto = Console.ReadLine();
+            Tokenizer Prueba = new Tokenizer(texto);
+            Prueba.Show_TokenSet();
+            
+        }
+
+    }
+}
